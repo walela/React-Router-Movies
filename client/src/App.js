ce4cf99fb16c4b22fff48f6b35228593a2523590
Austin Walela <austinmasibo@gmail.com>
2019-09-18 14:33:15 +0300

    set up index page Route torender MovieList

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route path="/" component={MovieList} />
     </div>
   );
 };
